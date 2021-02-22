ad5db26b8217295f370599d5e6da8e09d07c5d53
krishna9966 <krishna.9966426434@gmail.com>
2021-02-22 10:15:55 +0530

    codecov added

--- src/account/tests/test_authentication.py
+++ src/account/tests/test_authentication.py
@@ -76,10 +76,10 @@ class RegisterTest(BaseTest):
 
 class LoginTest(BaseTest):
     def test_can_access_page(self):
-        response = self.client.get(self.login_url)
+        response=self.client.get(self.login_url)
         self.assertEqual(response.status_code,200)
         self.assertTemplateUsed(response,'account/login.html')
-    
+        
     # def test_login_success(self):
     #     self.client.post(self.register_url,self.user,format='text/html')
     #     user=User.objects.filter(email=self.user['email']).first()
@@ -87,26 +87,16 @@ class LoginTest(BaseTest):
     #     user.save()
     #     response= self.client.post(self.login_url,self.user,format='text/html')
     #     self.assertEqual(response.status_code,302)
-    # def test_login_success(self):
-    #     self.client.post(self.register_url,self.user,format='text/html')
-    #     user = User.objects.filter(email= self.user['email']).first()
-    #     user.is_active = True
-    #     user.save()
-    #     response = self.client.post(self.login_url,self.user,format='text/html')
-    #     self.assertEqual(response.status_code,302)
-
-    def test_cant_login_with_unverified_email(self):
-        self.client.post(self.register_url,self.user,format='text/html')
-        
-        response = self.client.post(self.login_url,self.user,format='text/html')
-        self.assertEqual(response.status_code,401)
-
-    def test_cant_login_with_no_username(self):
-        
-        response = self.client.post(self.login_url,{'password':'password','username':''},format='text/html')
-        self.assertEqual(response.status_code,401)
     
-    def test_cant_login_with_no_username(self):
+    def test_cantlogin_with_unverified_email(self):
+        self.client.post(self.register_url,self.user,format='text/html')
+        response= self.client.post(self.login_url,self.user,format='text/html')
+        self.assertEqual(response.status_code,401)
+
+    def test_cantlogin_with_no_username(self):
+        response= self.client.post(self.login_url,{'password':'passwped','username':''},format='text/html')
+        self.assertEqual(response.status_code,401)
         
-        response = self.client.post(self.login_url,{'password':'','username':'username'},format='text/html')
+    def test_cantlogin_with_no_password(self):
+        response= self.client.post(self.login_url,{'username':'passwped','password':''},format='text/html')
         self.assertEqual(response.status_code,401)
